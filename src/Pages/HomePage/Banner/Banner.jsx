6b5ba4619aff9be7ee6dc6dee27c59d1f5bafa4a
MAHAFUJUR RAHMAN MUNNA <mahafujurrahman4480@gmail.com>
2026-04-15 18:23:48 +0600

    implement dynamic friend statistics in Banner component

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+
 import { FaPlus } from 'react-icons/fa';
+import useFriendsData from '../../../Hook/useFriendsData';
 
 const Banner = () => {
+const {data} = useFriendsData()
+const totalFriends = data.length;
+const onTrack = data.filter(friend => friend.status === "on_track")
+const needAttention = data.filter(friend => friend.status === "overdue")
+
     return (
         <div >
             <div className='flex items-center justify-center flex-col space-y-2 py-10'>
@@ -16,15 +22,15 @@ const Banner = () => {
             </div>
             <div className='grid md:grid-cols-2 lg:grid-cols-4  gap-5 pb-10'>
                 <div className='flex flex-col items-center justify-center p-5  bg-white rounded-xl'> 
-                    <h1 className='font-bold'>8</h1>
+                    <h1 className='font-bold'>{totalFriends}</h1>
                     <p className='text-zinc-500'>Total Friends</p>
                  </div>
                 <div className='flex flex-col items-center justify-center p-5 bg-white rounded-xl'> 
-                    <h1 className='font-bold'>3</h1>
+                    <h1 className='font-bold'>{onTrack.length}</h1>
                     <p className='text-zinc-500'>On Track</p>
                  </div>
                 <div className='flex flex-col items-center justify-center p-5 bg-white rounded-xl'>
-                    <h1 className='font-bold'>5</h1>
+                    <h1 className='font-bold'>{needAttention.length}</h1>
                     <p className='text-zinc-500'>Need Attention</p>
                 </div>
                 <div className='flex flex-col items-center justify-center p-5 bg-white rounded-xl'>
